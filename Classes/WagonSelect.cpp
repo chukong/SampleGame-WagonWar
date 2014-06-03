c982c72e215b1c87b1b36693e2c8c199af3905fc
Ryeeeeee <spatblan@gmail.com>
2014-06-03 21:18:25 +0800

    add animations

--- Classes/WagonSelect.cpp
+++ Classes/WagonSelect.cpp
@@ -16,6 +16,8 @@
 
 USING_NS_CC;
 
+extern GameConfig g_gameConfig;
+
 WagonSelect* WagonSelect::create(TurnType turntype)
 {
     WagonSelect *pRet = new WagonSelect();
@@ -76,22 +78,24 @@ void WagonSelect::createUI()
     this->addChild(wagon_bk,1);
     
     //boy or girl
-    auto boy_memuitem = MenuItemImage::create("boy_0.png","boy_1.png",
+    _boy_memuitem = MenuItemImage::create("boy_0.png","boy_1.png",
                                                   CC_CALLBACK_1(WagonSelect::boy_selected_callback, this));
-    boy_memuitem->setAnchorPoint(Point::ANCHOR_MIDDLE);
-    boy_memuitem->setPosition(Point(60,g_visibleRect.visibleHeight/2+60));
+    _boy_memuitem->setAnchorPoint(Point::ANCHOR_MIDDLE);
+    _boy_memuitem->setPosition(Point(60,g_visibleRect.visibleHeight/2+60));
+    _boy_memuitem->selected();
     
-    auto girl_memuitem = MenuItemImage::create("girl_0.png","girl_1.png",
+    _girl_memuitem = MenuItemImage::create("girl_0.png","girl_1.png",
                                                  CC_CALLBACK_1(WagonSelect::girl_selected_callback, this));
-    girl_memuitem->setAnchorPoint(Point::ANCHOR_MIDDLE);
-    girl_memuitem->setPosition(Point(60,g_visibleRect.visibleHeight/2-60));
+    _girl_memuitem->setAnchorPoint(Point::ANCHOR_MIDDLE);
+    _girl_memuitem->setPosition(Point(60,g_visibleRect.visibleHeight/2-60));
     
-    auto menu = Menu::create(boy_memuitem, girl_memuitem, nullptr);
+    auto menu = Menu::create(_boy_memuitem, _girl_memuitem, nullptr);
     menu->setPosition(Point::ZERO);
     this->addChild(menu,1);
     
+    //
     MenuItemImage* ready_menu;
-    if (_turntype == FIRSR_TURN) {
+    if (_turntype == FIRST_TURN) {
         ready_menu = MenuItemImage::create("btn_ready_1.png","btn_ready_2.png",CC_CALLBACK_1(WagonSelect::ready_callback, this));
     }
     else if(_turntype == SECOND_TURN) {
@@ -102,29 +106,74 @@ void WagonSelect::createUI()
     menu_ready->setPosition(Point::ZERO);
     this->addChild(menu_ready,1);
     
-    auto wagon1_menuitem = MenuItemImage::create("wagon_item_0.png","wagon_item_1.png",
+    //
+    _wagon1_menuitem = MenuItemImage::create("wagon_item_0.png","wagon_item_1.png",
                                                  CC_CALLBACK_1(WagonSelect::wagon1_selected_callback, this));
-    wagon1_menuitem->setAnchorPoint(Point::ANCHOR_MIDDLE);
-    wagon1_menuitem->setPosition(Point(120,g_visibleRect.visibleHeight/2-80));
+    _wagon1_menuitem->setAnchorPoint(Point::ANCHOR_MIDDLE);
+    _wagon1_menuitem->setPosition(Point(120,g_visibleRect.visibleHeight/2-80));
+    _wagon1_menuitem->selected();
     
-    auto wagon2_menuitem = MenuItemImage::create("wagon_item_0.png","wagon_item_1.png",
+    _wagon2_menuitem = MenuItemImage::create("wagon_item_0.png","wagon_item_1.png",
                                                  CC_CALLBACK_1(WagonSelect::wagon2_selected_callback, this));
-    wagon2_menuitem->setAnchorPoint(Point::ANCHOR_MIDDLE);
-    wagon2_menuitem->setPosition(Point(310,g_visibleRect.visibleHeight/2-80));
+    _wagon2_menuitem->setAnchorPoint(Point::ANCHOR_MIDDLE);
+    _wagon2_menuitem->setPosition(Point(310,g_visibleRect.visibleHeight/2-80));
     
-    auto wagon3_menuitem = MenuItemImage::create("wagon_item_0.png","wagon_item_1.png",
+    _wagon3_menuitem = MenuItemImage::create("wagon_item_0.png","wagon_item_1.png",
                                                  CC_CALLBACK_1(WagonSelect::wagon3_selected_callback, this));
-    wagon3_menuitem->setAnchorPoint(Point::ANCHOR_MIDDLE);
-    wagon3_menuitem->setPosition(Point(500,g_visibleRect.visibleHeight/2-80));
+    _wagon3_menuitem->setAnchorPoint(Point::ANCHOR_MIDDLE);
+    _wagon3_menuitem->setPosition(Point(500,g_visibleRect.visibleHeight/2-80));
     
-    auto wagon4_menuitem = MenuItemImage::create("wagon_item_0.png","wagon_item_1.png",
+    _wagon4_menuitem = MenuItemImage::create("wagon_item_0.png","wagon_item_1.png",
                                                  CC_CALLBACK_1(WagonSelect::wagon4_selected_callback, this));
-    wagon4_menuitem->setAnchorPoint(Point::ANCHOR_MIDDLE);
-    wagon4_menuitem->setPosition(Point(690,g_visibleRect.visibleHeight/2-80));
+    _wagon4_menuitem->setAnchorPoint(Point::ANCHOR_MIDDLE);
+    _wagon4_menuitem->setPosition(Point(690,g_visibleRect.visibleHeight/2-80));
     
-    auto menu_wagon = Menu::create(wagon1_menuitem, wagon2_menuitem, wagon3_menuitem, wagon4_menuitem, nullptr);
+    auto menu_wagon = Menu::create(_wagon1_menuitem, _wagon2_menuitem, _wagon3_menuitem, _wagon4_menuitem, nullptr);
     menu_wagon->setPosition(Point::ZERO);
     wagon_bk->addChild(menu_wagon,1);
+    
+    //
+    auto wagon1 = Sprite::createWithSpriteFrame(SpriteFrameCache::getInstance()->getSpriteFrameByName("cnm_idle01.png"));
+    wagon1->runAction(RepeatForever::create(g_gameConfig.getAnimate(g_gameAnimation.cnm_idle)));
+    wagon1->setAnchorPoint(Point::ANCHOR_MIDDLE);
+    wagon1->setPosition(Point(120,g_visibleRect.visibleHeight/2-20));
+    wagon1->setScale(1.5f);
+    wagon_bk->addChild(wagon1, 2);
+    
+    auto wagon2 = Sprite::createWithSpriteFrame(SpriteFrameCache::getInstance()->getSpriteFrameByName("tankidle01.png"));
+    wagon2->runAction(RepeatForever::create(g_gameConfig.getAnimate(g_gameAnimation.tank_idle)));
+    wagon2->setAnchorPoint(Point::ANCHOR_MIDDLE);
+    wagon2->setPosition(Point(310,g_visibleRect.visibleHeight/2-20));
+    wagon2->setScale(1.5f);
+    wagon_bk->addChild(wagon2, 2);
+    
+    auto wagon3 = Sprite::createWithSpriteFrame(SpriteFrameCache::getInstance()->getSpriteFrameByName("rockidle01.png"));
+    wagon3->runAction(RepeatForever::create(g_gameConfig.getAnimate(g_gameAnimation.rock_idle)));
+    wagon3->setAnchorPoint(Point::ANCHOR_MIDDLE);
+    wagon3->setPosition(Point(500,g_visibleRect.visibleHeight/2-10));
+    wagon3->setScale(1.5f);
+    wagon_bk->addChild(wagon3, 2);
+    
+    auto wagon4_1 = Sprite::createWithSpriteFrame(SpriteFrameCache::getInstance()->getSpriteFrameByName("mechidle01.png"));
+    wagon4_1->runAction(RepeatForever::create(g_gameConfig.getAnimate(g_gameAnimation.mech_idle)));
+    wagon4_1->setAnchorPoint(Point::ANCHOR_MIDDLE);
+    wagon4_1->setPosition(Point(700,g_visibleRect.visibleHeight/2-20));
+    wagon4_1->setScale(1.5f);
+    wagon_bk->addChild(wagon4_1, 2);
+    
+    auto wagon4_2 = Sprite::createWithSpriteFrame(SpriteFrameCache::getInstance()->getSpriteFrameByName("mechgunidle01.png"));
+    wagon4_2->runAction(RepeatForever::create(g_gameConfig.getAnimate(g_gameAnimation.mechgun_idle)));
+    wagon4_2->setAnchorPoint(Point::ANCHOR_MIDDLE);
+    wagon4_2->setPosition(Point(700,g_visibleRect.visibleHeight/2-20));
+    wagon4_2->setScale(1.5f);
+    wagon_bk->addChild(wagon4_2, 2);
+
+    //flash
+    flash = Sprite::create("wagon_flash.png");
+    flash->setAnchorPoint(Point::ANCHOR_MIDDLE);
+    flash->setPosition(Point(120,g_visibleRect.visibleHeight/2-80));
+    flash->runAction(RepeatForever::create(Blink::create(1,1)));
+    wagon_bk->addChild(flash,3);
 }
 
 void WagonSelect::ready_callback(Ref* ref)
@@ -133,7 +182,7 @@ void WagonSelect::ready_callback(Ref* ref)
     notouchlayer->setTag(NOTOUCHTAG);
     this->addChild(notouchlayer,100);
     
-    if (_turntype == FIRSR_TURN) {
+    if (_turntype == FIRST_TURN) {
         setup_player1_mactchdata();
         GPGSManager::TakeTurn(false, false);
     }
@@ -187,30 +236,62 @@ void WagonSelect::setup_player2_mactchdata()
 
 void WagonSelect::boy_selected_callback(Ref* ref)
 {
-    _isBoy = true;
+    if(!_isBoy){
+        _boy_memuitem->selected();
+        _girl_memuitem->unselected();
+        _isBoy = true;
+    }
 }
 
 void WagonSelect::girl_selected_callback(Ref* ref)
 {
-    _isBoy = false;
+    if(_isBoy){
+        _boy_memuitem->unselected();
+        _girl_memuitem->selected();
+        _isBoy = false;
+    }
 }
 
 void WagonSelect::wagon1_selected_callback(cocos2d::Ref* ref)
 {
+    _wagon1_menuitem->selected();
+    _wagon2_menuitem->unselected();
+    _wagon3_menuitem->unselected();
+    _wagon4_menuitem->unselected();
+    
+    flash->setPosition(_wagon1_menuitem->getPosition());
     _wagon = 0;
 }
 
 void WagonSelect::wagon2_selected_callback(cocos2d::Ref* ref)
 {
+    _wagon1_menuitem->unselected();
+    _wagon2_menuitem->selected();
+    _wagon3_menuitem->unselected();
+    _wagon4_menuitem->unselected();
+    
+    flash->setPosition(_wagon2_menuitem->getPosition());
     _wagon = 1;
 }
 
 void WagonSelect::wagon3_selected_callback(cocos2d::Ref* ref)
 {
+    _wagon1_menuitem->unselected();
+    _wagon2_menuitem->unselected();
+    _wagon3_menuitem->selected();
+    _wagon4_menuitem->unselected();
+    
+    flash->setPosition(_wagon3_menuitem->getPosition());
     _wagon = 2;
 }
 
 void WagonSelect::wagon4_selected_callback(cocos2d::Ref* ref)
 {
+    _wagon1_menuitem->unselected();
+    _wagon2_menuitem->unselected();
+    _wagon3_menuitem->unselected();
+    _wagon4_menuitem->selected();
+    
+    flash->setPosition(_wagon4_menuitem->getPosition());
     _wagon = 3;
 }
