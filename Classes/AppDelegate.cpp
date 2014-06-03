99c7cd0a6b1298cd412c0edce6fb1ade516de7e3
Ryeeeeee <spatblan@gmail.com>
2014-06-03 22:21:21 +0800

    add dude & wagon in wagonselect.cpp

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -44,9 +44,9 @@ bool AppDelegate::applicationDidFinishLaunching() {
     
     // create a scene. it's an autorelease object
   //   auto scene = GameScene::createScene();
-      auto scene = MainScreenScene::createScene();
+      //auto scene = MainScreenScene::createScene();
   //    auto scene = TestTBMP::createScene("345678");
-    //auto scene = WagonSelect::createScene(FIRST_TURN);
+    auto scene = WagonSelect::createScene(FIRST_TURN);
     // run
     director->runWithScene(scene);
     
