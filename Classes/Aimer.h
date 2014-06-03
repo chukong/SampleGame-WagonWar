1dd646dba7a6cf98ca871272105539a5291ba5bc
Wu-Hao <wuhao@cocos2d-x.org>
2014-06-04 00:48:50 +0800

    finished aimer

--- Classes/Aimer.h
+++ Classes/Aimer.h
@@ -19,10 +19,19 @@ public:
     float upperLimit;
     float lowerLimit;
     Aimer():upperLimit(-45), lowerLimit(-10){};
+    void setAngle(float a);
+    float getWorldAngle();
+
 protected:
     cocos2d::Sprite* _back;
     cocos2d::Sprite* _pointer;
     cocos2d::ProgressTimer* _green;
+    cocos2d::Sprite* _crosshair;
+    float _angle;
+    
+    bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* event);
+    void onTouchMoved(cocos2d::Touch* touch, cocos2d::Event* event);
+    void onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* event);
 };
 
 #endif /* defined(__TankMultiplayer__Aimer__) */
